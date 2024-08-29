9aa61922554da7bb84b038f91e703687efdef70d
arun babu <ntarunbabu@gmail.com>
2024-08-29 20:23:01 +0100

    added styles to the view and add product pages. implements stylesheet using SASS. implemented styles through partials for better organization of the stylesheet.

--- Public/JS/productType.js
+++ Public/JS/productType.js
@@ -6,35 +6,64 @@ document.getElementById('productType').addEventListener('change', (event)=>{
 
     if(selectedType === 'DVD'){
         productSpecific.innerHTML = `
-       <div class="form-group">
-            <label for="size">Size (MB)</label>
-            <input type="number" id="size" name="size" required>
-            <p>Please, provide size.</p>
+         <div class="form-group">
+            <div class="form-field">
+                <label for="size">Size (MB)</label>
+                <input type="number" id="size" name="size" required>
+            </div>
+            <div class="error-class">
+                <p>Please, provide size.</p>
+                <span class="error" id="size-error">Size is required</span>
+            </div>   
         </div>
         `;
     }else if(selectedType === 'Book'){
         productSpecific.innerHTML = `
-        <div class="form-group">
-            <label for="weight">Weight (KG)</label>
-            <input type="number" id="weight" name="weight" required>
-            <p>Please, provide weight.</p>
-        </div>
+           <div class="form-group">
+                <div class="form-field">
+                    <label for="weight">Weight (KG)</label>
+                    <input type="number" id="weight" name="weight" required>
+                </div>
+                <div class="error-class">
+                    <p>Please, provide weight.</p>
+                    <span class="error" id="weight-error">Weight is required</span>
+                </div>   
+            </div>
     `;
     }else if(selectedType === 'Furniture'){
         productSpecific.innerHTML = `
-            <div class="form-group">
-                <label for="height">Height (CM)</label>
-                <input type="number" id="height" name="height" required>
+              <div class="form-group">
+                <div class="form-field">
+                    <label for="height">Height (CM)</label>
+                    <input type="number" id="height" name="height" required>
+                </div>
+                <div class="error-class">
+                    <span class="error" id="height-error">Height is required</span>
+                </div>   
             </div>
+
             <div class="form-group">
-                <label for="width">Width (CM)</label>
-                <input type="number" id="width" name="width" required>
+                <div class="form-field">
+                    <label for="width">Width (CM)</label>
+                    <input type="number" id="width" name="width" required>
+                </div>
+                <div class="error-class">
+                    <span class="error" id="width-error">Width is required</span>
+                </div>   
             </div>
+
             <div class="form-group">
-                <label for="length">Length (CM)</label>
-                <input type="number" id="length" name="length" required>
+                <div class="form-field">
+                    <label for="length">Length (CM)</label>
+                    <input type="number" id="length" name="length" required>
+                </div>
+                <div class="error-class">
+                    <span class="error" id="length-error">Length is required</span>
+                </div>   
             </div>
-            <p>Please, provide dimensions.</p>
+            <div id="select-informations">
+                <p>Please, provide dimensions.</p>
+            </div>  
         `;
     }
 });
